6f523ec3bb9d481bd3c845bd31808c035781fe8a
sukhendu <sukhenduguria2017@gmail.com>
2022-10-30 17:03:54 +0530

    connect mongodb

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
